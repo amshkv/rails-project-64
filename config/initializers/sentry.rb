0fe65731de24c4116c1b7d92df30b1df6e35218a
Andrey Moshkov <fakesupp@gmail.com>
2022-11-18 23:27:22 +0300

    :heavy_plus_sign: add sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  # NOTE: такой хитрый обход чтобы сентри не запускалась на dev машине, а только на проде, где есть DSN переменная
+  config.dsn = ENV.fetch('SENTRY_DSN', nil)
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
