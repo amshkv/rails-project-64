ba4a52176e0962dbb278b53e43343145652b95d1
Andrey Moshkov <fakesupp@gmail.com>
2022-11-19 23:23:41 +0300

    :heavy_plus_sign: add sentry to frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,16 @@
 // Entry point for the build script in your package.json
+
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
+
 import '@hotwired/turbo-rails';
 import './controllers';
 import 'bootstrap';
+
+Sentry.init({
+  dsn: 'https://1270b892d6254a018b19bef9475f4b6d@o4504181930721280.ingest.sentry.io/4504181933932544',
+
+  integrations: [new BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
