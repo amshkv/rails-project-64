a8e0fe0cf5d6f1783bf2b0b8b24c2fa5c98a4694
Andrey Moshkov <fakesupp@gmail.com>
2022-12-16 22:26:18 +0300

    :sparkles: add posts resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,54 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    params = { creator: current_user }.merge(permitted_params)
+    @post = Post.new(params)
+
+    if @post.save
+      redirect_to @post, notice: I18n.t('posts.create.success')
+    else
+      render :new, status: :unprocessable_entity, alert: I18n.t('posts.create.failure')
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(permitted_params)
+      redirect_to @post, notice: I18n.t('posts.update.success')
+    else
+      render :edit, status: :unprocessable_entity, alert: I18n.t('posts.update.failure')
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to root_path, notice: I18n.t('posts.destroy.success')
+  end
+
+  private
+
+  def permitted_params
+    params.require(:post).permit(:title, :body, :category_id)
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,8 +3,62 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = posts(:one)
+    @user = users(:harry)
+    sign_in(@user)
+
+    @attrs = {
+      title: Faker::Movies::HarryPotter.book,
+      body: Faker::Movies::HarryPotter.quote,
+      category_id: categories(:spells).id
+    }
+  end
+
   test 'should get index' do
     get posts_url
     assert_response :success
+  end
+
+  test 'should get show' do
+    get post_url(@post)
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_post_url
+    assert_response :success
+  end
+
+  test 'should get create' do
+    post posts_url, params: { post: @attrs }
+    assert_response :redirect
+
+    post = Post.find_by(title: @attrs[:title])
+    assert { post }
+  end
+
+  test 'should get edit' do
+    get edit_post_url(@post)
+
+    assert_response :success
+  end
+
+  test 'should get update' do
+    patch post_url(@post), params: { post: @attrs }
+
+    assert_response :redirect
+
+    @post.reload
+
+    assert { @post.title == @attrs[:title] }
+    assert { @post.body == @attrs[:body] }
+  end
+
+  test 'should get destroy' do
+    delete post_url(@post)
+    assert_response :redirect
+
+    assert { !Post.find_by(id: @post.id) }
   end
 end
