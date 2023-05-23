178fc664333f0005186c64646db0592ee54baa37
Andrey Moshkov <fakesupp@gmail.com>
2023-05-24 00:24:18 +0300

    :coffin: Remove unused bullet settings code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,21 +4,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-# NOTE: честно украдено с https://gist.github.com/kerrizor/8bb8f1528c88789c3435
-module BulletHelper
-  def before_setup
-    Bullet.start_request
-    super if defined?(super)
-  end
-
-  def after_teardown
-    super if defined?(super)
-
-    Bullet.perform_out_of_channel_notifications if Bullet.notification?
-    Bullet.end_request
-  end
-end
-
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -28,7 +13,6 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
-    include BulletHelper
   end
 end
 
