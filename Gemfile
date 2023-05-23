0f0082c778563b65ca4c0ae317304d148b5a87ba
Andrey Moshkov <fakesupp@gmail.com>
2023-05-23 23:44:02 +0300

    :sparkles: Add ancestry comments

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'bootsnap', require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem 'ancestry'
+
 gem 'sentry-rails'
 gem 'sentry-ruby'
 
--- test/models/post_comment_test.rb
+++ test/models/post_comment_test.rb
@@ -5,6 +5,7 @@
 # Table name: post_comments
 #
 #  id         :integer          not null, primary key
+#  ancestry   :string
 #  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -13,8 +14,9 @@
 #
 # Indexes
 #
-#  index_post_comments_on_post_id  (post_id)
-#  index_post_comments_on_user_id  (user_id)
+#  index_post_comments_on_ancestry  (ancestry)
+#  index_post_comments_on_post_id   (post_id)
+#  index_post_comments_on_user_id   (user_id)
 #
 # Foreign Keys
 #
