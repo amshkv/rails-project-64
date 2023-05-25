8dbbbd9186852569ca2da4bd282956950441ebe9
Andrey Moshkov <fakesupp@gmail.com>
2023-05-25 08:57:51 +0300

    :recycle: Improve errors on create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  def create # rubocop:disable Metrics/AbcSize
+  def create
     @comment = current_user.comments.build(permitted_params)
     @comment.post = resource_post
     if @comment.save
-      # TODO: разобраться как тут показывать ошибки у этой формы, если валидация не прошла или что-то пошло не так
-      # кроме флешек?
       redirect_to resource_post, notice: I18n.t('comments.create.success')
     else
-      @post = resource_post
-      # TODO: иначе падает ошибка в кривом комменте, мб лучше `&` во вьюхе?, еще и рубокоп ругается на сложность
-      @root_post_comments = @post.comments.roots
-      flash[:alert] = I18n.t('comments.create.failure')
-      redirect_to resource_post # TODO: если делать render, то падает что не может найти partial
+      redirect_to resource_post, status: :unprocessable_entity, alert: I18n.t('comments.create.failure')
     end
   end
 
