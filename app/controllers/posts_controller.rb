b06d1646183d75ce57d150c674b3bdd59315bf1e
Andrey Moshkov <fakesupp@gmail.com>
2023-05-25 22:34:47 +0300

    :lipstick: Add date and author to post on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :only_creator, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.includes([:creator])
   end
 
   def show
